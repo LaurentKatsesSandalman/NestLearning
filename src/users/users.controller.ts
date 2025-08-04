2d9b95d2023a966b868eaebcd4fa3bbe9ae2771a
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-08-04 18:08:22 +0200

    night commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity/user.entity';
 
 @Controller('/users')
-export class UsersController {}
+export class UsersController {
+
+constructor(private readonly userService: UsersService){}
+
+@Get(':id')
+getUserbyId(@Param() params){
+    return this.userService.getUser(params.id);
+}
+
+@Post()
+createUser(@Body() user: User){
+    return this.userService.saveUser(user);
+}
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ temp.sql
@@ -0,0 +1,6 @@
+-- Active: 1747815168837@@127.0.0.1@3306@nestlearning
+Drop Table user
+
+CREATE DATABASE nestlearning
+
+USE nestlearning;
